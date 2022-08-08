a7bdf03cc8f0ee51e2e08675e2a46cd7738278c3
AlwaysUsePython <98504827+AlwaysUsePython@users.noreply.github.com>
2022-08-08 11:52:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -170,7 +170,156 @@ def main():
         pygame.display.update()
 
 
+colorDictionary = {"background": (152, 140, 132),
+                   " ": (188,173,159),
+                   "2": (238, 228, 218),
+                   "4": (238, 225, 201),
+                   "8": (243,178,122),
+                   "16": (247, 149, 100),
+                   "32":(247,125,95),
+                   "64": (248,92,60),
+                   "128+":(237,208,115),
+                   "blackText":(119,110,101),
+                   "whiteText":(248, 246, 242)
+                   }
 
+textColorDictionary = {
+  " " : colorDictionary["blackText"],
+  "2" : colorDictionary["blackText"],
+  "4" : colorDictionary["blackText"],
+  "8+" : colorDictionary["whiteText"]
+}
+
+font = pygame.font.Font("freesansbold.ttf", 60)
+
+
+def copyList(list):
+    newList = []
+    for item in list:
+        newList.append(item)
+    return newList
+
+
+class Tile:
+    def __init__(self, x, y):
+        self.color = colorDictionary[" "]
+        self.value = " "
+        self.x = x
+        self.y = y
+        self.textColor = colorDictionary["blackText"]
+
+    def setValue(self, valueStr):
+        self.value = valueStr
+        try:
+            self.color = colorDictionary[valueStr]
+        except:
+            self.color = colorDictionary["128+"]
+
+        try:
+            self.textColor = textColorDictionary[valueStr]
+        except:
+            self.textColor = textColorDictionary["8+"]
+
+    def doubleValue(self):
+
+        if self.value == " ":
+            self.value = "2"
+
+            self.color = colorDictionary["2"]
+
+            self.textColor = textColorDictionary["2"]
+
+            return
+
+        self.value = str(int(self.value) * 2)
+
+        if self.value == "8":
+            self.textColor = colorDictionary["whiteText"]
+
+            self.color = colorDictionary["8"]
+
+        elif int(self.value) >= 128 and int(self.value) < 4096:
+            self.color = colorDictionary["128+"]
+
+        elif int(self.value) >= 4096:
+            self.color = colorDictionary[" "]
+
+            self.value = " "
+            return
+
+        else:
+            try:
+                self.color = colorDictionary[self.value]
+            except:
+                self.color = colorDictionary["empty tile"]
+
+    def draw(self, screen):
+        # I added a few more parameters to the rectangle command, don't worry about them. They just make the corners rounded
+
+        pygame.draw.rect(screen, self.color, pygame.Rect(self.x - 70, self.y - 70, 140, 140), border_top_left_radius=10,
+                         border_top_right_radius=10, border_bottom_left_radius=10, border_bottom_right_radius=10)
+
+        text = font.render(self.value, True, self.textColor)
+        textRect = text.get_rect()
+        textRect.center = (self.x, self.y)
+        screen.blit(text, textRect)
+
+
+class Game:
+    def __init__(self):
+        self.board = []
+
+        currentX = 90
+        currentY = 90
+
+        for r in range(4):
+            row = []
+            for t in range(4):
+                row.append(Tile(currentX, currentY))
+                currentX += 160
+            self.board.append(copyList(row))
+            currentY += 160
+            currentX = 90
+
+        self.score = 0
+
+
+    def draw(self, screen):
+        screen.fill(colorDictionary["background"])
+        for row in self.board:
+            for tile in row:
+                tile.draw(screen)
+
+
+
+def create2048():
+    screen = pygame.display.set_mode((660, 660))
+
+    game = Game()
+
+    running = True
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            elif event.type == pygame.KEYDOWN and pygame.key == pygame.K_ESCAPE:
+                running = False
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+
+                row = pos[1] // 160
+                col = pos[0] // 160
+
+                game.board[row][col].doubleValue()
+
+
+
+
+        game.draw(screen)
+        pygame.display.update()
 
 
 
@@ -227,6 +376,50 @@ def othello():
                     board[row][col] = "green"
 
         drawOthelloBoard(board, screen)
+
+        pygame.display.update()
+
+def main():
+
+    screen = pygame.display.set_mode((500, 500))
+
+    buttons = []
+
+    buttons.append(Button(250, 100, 400, 100, "Connect Four"))
+
+    buttons.append(Button(250, 250, 400, 100, "Othello"))
+
+    buttons.append(Button(250, 400, 400, 100, "2048"))
+
+    running = True
+
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+
+                pos = pygame.mouse.get_pos()
+
+                for button in buttons:
+                    if button.detectPress(pos[0], pos[1]):
+                        if button.text == "Connect Four":
+                            connect4()
+                        elif button.text == "Othello":
+                            othello()
+                        elif button.text == "2048":
+                            create2048()
+
+        screen.fill((100, 100, 100))
+
+        for button in buttons:
+            button.draw(screen)
 
         pygame.display.update()
 
